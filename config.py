1dfaf1f83cd2fe7f1ad3a3798fbd029434fce9e0
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-23 11:55:51 +0300

    create the application startup files

---
+++ config.py
@@ -0,0 +1,25 @@
+class Config:
+    """
+    General configuration class
+    """
+    pass
+
+
+class ProdConfig(Config):
+    """
+    Production configuration class
+    """
+    pass
+
+
+class DevConfig(Config):
+    """
+    Development configuration class
+    """
+    DEBUG = True
+
+
+config_options = {
+    'production': ProdConfig,
+    'development': DevConfig
+}
