9cde35ce17d0a14cd6c99b09322915340ee724b0
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-25 13:36:38 +0300

    add sign in and sign up

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,16 @@
 from flask import render_template
 from . import main
+from flask_login import login_required
 
 
 @main.route('/')
 def index():
     title = 'Home - Welcome to The Pitches Website'
     return render_template('index.html', title=title)
+
+
+@main.route('/pitches')
+@login_required
+def pitches():
+    title = 'Pitches -  Welcome to The Pitches Website'
+    return render_template('pitches.html', title=title)
