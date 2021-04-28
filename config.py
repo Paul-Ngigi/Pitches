841bc600d3d58306efb7d5696b1f0d1b15f3c40a
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-28 12:29:38 +0300

    update the config file

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class ProdConfig(Config):
     """
     Production configuration class
     """
-    SQLALCHEMY_DATABASE_URI = "postgresql://uafyzmmvcusmlk:8e384a8b4a2aeb83b754ec59184d64504bf720cca1b49896747248f5e16695f2@ec2-3-233-43-103.compute-1.amazonaws.com:5432/d9tfm57fdoaa1l?sslmode=require"
+    SQLALCHEMY_DATABASE_URI = 'postgresql://pykonncljnjurg:ca9419f42cf10926d49c52fd2a8e7b5e3c63b3d4d9b419d32a77c3b4d04c801c@ec2-18-214-140-149.compute-1.amazonaws.com:5432/dekiu1uqvid4ta?sslmode=require'
 
 
 class DevConfig(Config):
