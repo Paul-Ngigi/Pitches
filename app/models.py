91f0b9d78c34e67af668df6f3c18df22f4a62bbd
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-28 10:00:35 +0300

    update the model

--- app/models.py
+++ app/models.py
@@ -6,9 +6,9 @@ from werkzeug.security import generate_password_hash,check_password_hash
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(255),unique = True,nullable = True)
-    email  = db.Column(db.String(255),unique = True,nullable = True)
-    secure_password = db.Column(db.String(255),nullable = True)
+    username = db.Column(db.String(255),unique = True)
+    email  = db.Column(db.String(255),unique = True)
+    secure_password = db.Column(db.String(255))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
