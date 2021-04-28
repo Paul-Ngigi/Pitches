1d18534858d5d035259f14d78642c62b3435b296
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-28 15:10:17 +0300

    make changes to the email

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class Config:
     """
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://root:root@localhost/pitches'
-    SQLALCHEMY_TRACK_MODIFICATIONS=True
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
     # Email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
@@ -23,6 +23,11 @@ class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://pykonncljnjurg:ca9419f42cf10926d49c52fd2a8e7b5e3c63b3d4d9b419d32a77c3b4d04c801c@ec2-18-214-140-149.compute-1.amazonaws.com:5432/dekiu1uqvid4ta?sslmode=require'
 
 
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://root:root@localhost/pitches_test'
+
+
+
 class DevConfig(Config):
     """
     Development configuration class
@@ -30,14 +35,11 @@ class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://root:root@localhost/pitches'
     DEBUG = True
     
-    
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://root:root@localhost/pitches_test'
 
 
 
 config_options = {
-    'production': ProdConfig,
     'development': DevConfig,
-    'test':TestConfig
+    'production': ProdConfig,
+    'test': TestConfig
 }
